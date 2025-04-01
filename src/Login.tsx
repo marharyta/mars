5ff4318a7114e955daf799940faf9df3a3e7c0d9
Margo Roi <marharyta.obraztsova@gmail.com>
2025-04-02 01:18:48 +0300

    feat: update login page to comply with ant design guidelines

--- src/Login.tsx
+++ src/Login.tsx
@@ -3,11 +3,12 @@ import { useAuth } from "./auth/AuthProvider";
 import { setUserAtom } from "./atoms/user";
 import { useSetAtom } from "jotai";
 import type { LoginProps } from "./types";
-import { Input, Button, Typography, Form, Layout, Card } from "antd";
-
+import { Layout, Button, Typography, Form, Input, Card } from "antd";
 const { Title } = Typography;
+const { Content } = Layout;
 
 export const Login = ({ onLoginSuccess }: LoginProps) => {
+  //TODO: remove hardcoded values
   const [user_id, setUserId] = useState("alice");
   const [password, setPassword] = useState("1234");
   const { login } = useAuth();
@@ -15,6 +16,7 @@ export const Login = ({ onLoginSuccess }: LoginProps) => {
   const setUser = useSetAtom(setUserAtom);
 
   const handleSubmit = async (e: React.FormEvent) => {
+    //TODO: fix that, git broken after introduction of ant design
     e.preventDefault();
     try {
       setUser({ user_id: user_id, name: "", password: "" });
@@ -26,36 +28,38 @@ export const Login = ({ onLoginSuccess }: LoginProps) => {
   };
 
   return (
-    <Layout
-      style={{
-        minHeight: "100vh",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Card title={<Title level={2}>Login</Title>} style={{ width: 300 }}>
-        <Form layout="vertical" onFinish={handleSubmit}>
-          <Form.Item label="User ID" name="user_id">
-            <Input
-              value={user_id}
-              onChange={(e) => setUserId(e.target.value)}
-              placeholder="User ID"
-            />
-          </Form.Item>
-          <Form.Item label="Password" name="password">
-            <Input.Password
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="Password"
-            />
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary" htmlType="submit" block>
-              Login
-            </Button>
-          </Form.Item>
-        </Form>
-      </Card>
+    <Layout className="h-dvh">
+      <Content className="flex flex-col items-center aling-center justify-center">
+        <Card
+          title={
+            <Title level={2} className="font-bold">
+              login
+            </Title>
+          }
+        >
+          <Form layout="vertical" onFinish={handleSubmit}>
+            <Form.Item name="user_id">
+              <Input
+                value={user_id}
+                onChange={(e) => setUserId(e.target.value)}
+                placeholder="User ID"
+              />
+            </Form.Item>
+            <Form.Item name="password">
+              <Input.Password
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+              />
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit" block>
+                Login
+              </Button>
+            </Form.Item>
+          </Form>
+        </Card>
+      </Content>
     </Layout>
   );
 };
