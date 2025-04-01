88682fd86841383731af9430857556a5121f234f
Margo Roi <marharyta.obraztsova@gmail.com>
2025-04-02 01:17:54 +0300

    feat: add histogram

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import { useAuth } from "./auth/AuthProvider";
 import { useAtom, useSetAtom } from "jotai";
 import { tokenAtom } from "./atoms/auth";
@@ -6,29 +5,41 @@ import { setUserAtom, userAtom } from "./atoms/user";
 import type { DashboardProps, Ore } from "./types";
 import { Button, Typography, Layout, Card, List, Col, Row } from "antd";
 import { useQuery } from "@tanstack/react-query";
+import { OresHistogram } from "./OreHistogram";
 
 const { Title } = Typography;
 const { Header, Content } = Layout;
-const User = () => {
+
+const User = ({ children }: React.ReactElement) => {
   const [token] = useAtom(tokenAtom);
   const [user] = useAtom(userAtom);
   const setUser = useSetAtom(setUserAtom);
-  useEffect(() => {
-    if (user?.user_id) {
-      const fetchUsers = async () => {
-        const res = await fetch(`http://localhost:8080/users/${user.user_id}`, {
+
+  const {
+    data: userData,
+    isLoading: isLoadingUser,
+    error: errorFetchingUser,
+  } = useQuery({
+    queryKey: ["user", 1],
+    queryFn: async () => {
+      if (user?.user_id) {
+        const res = fetch(`http://localhost:8080/users/${user.user_id}`, {
           headers: {
             Authorization: `Bearer ${token}`,
           },
-        });
-        if (!res.ok) return;
-
-        const data = await res.json();
+        }).then((res) => res.json());
+        const data = await res;
         setUser(data);
-      };
-      fetchUsers();
-    }
-  }, [token, user, setUser]);
+        return data;
+      }
+    },
+  });
+
+  console.log("user", userData, isLoadingUser, errorFetchingUser);
+
+  // TODO: nice loading state + nice error state
+  if (errorFetchingUser) return <p>Error loading user</p>;
+  if (isLoadingUser) return <p>Is loading user</p>;
 
   return (
     <Card title="User Info">
@@ -38,12 +49,33 @@ const User = () => {
       <p>
         <strong>User ID:</strong> {user?.user_id}
       </p>
+      {children}
     </Card>
   );
 };
 
 const Scene = () => {
   return <div id="scene-container">Mars goes here</div>;
+};
+
+const DataList = ({ data }) => {
+  {
+    /*TODO: 
+        1. Sort by latest ascending
+        2. add react-virtualized later
+        3. Add UI
+      */
+  }
+  return (
+    <List
+      dataSource={data}
+      renderItem={(ore: Ore) => (
+        <List.Item>
+          {ore?.ore_sites} - {ore?.timestamp}
+        </List.Item>
+      )}
+    />
+  );
 };
 
 const Data = () => {
@@ -67,17 +99,18 @@ const Data = () => {
   if (isLoading) <p>Is loading aquisitions</p>;
 
   return (
-    <Card title="Acquisition Data">
-      {/*TODO: add react-virtualized later*/}
-      <List
-        dataSource={acquisitions}
-        renderItem={(ore: Ore) => (
-          <List.Item>
-            {ore?.ore_sites} - {ore?.timestamp}
-          </List.Item>
-        )}
-      />
-    </Card>
+    <>
+      <Col span={12}>
+        <Card title="Acquisition Data">
+          <OresHistogram data={acquisitions} />
+        </Card>
+      </Col>
+      <Col span={12}>
+        <Card title="Acquisition Data List">
+          <DataList data={acquisitions}></DataList>
+        </Card>
+      </Col>
+    </>
   );
 };
 
@@ -86,35 +119,33 @@ export const Dashboard = ({ onLogoutSuccess }: DashboardProps) => {
 
   return (
     <Layout>
-      <Header>
-        <Title style={{ color: "white" }} level={2}>
+      <Header className="flex flex-row justify-baseline items-baseline">
+        <Title level={2} className="text-xs">
           Dashboard
         </Title>
       </Header>
-      <Content style={{ padding: 24 }}>
-        <Row>
-          <Col span={6}>
-            <User />
-          </Col>
-          <Col span={6}>
-            <Data />
-          </Col>
+      <Content className="p-8">
+        <Row gutter={[16, 16]}>
+          <Data />
           <Col span={12}>
             <Scene />
           </Col>
+          <Col span={6}>
+            <User>
+              <Button
+                type="primary"
+                danger
+                onClick={async () => {
+                  await logout();
+                  onLogoutSuccess();
+                }}
+                style={{ marginTop: 20 }}
+              >
+                Logout
+              </Button>
+            </User>
+          </Col>
         </Row>
-
-        <Button
-          type="primary"
-          danger
-          onClick={async () => {
-            await logout();
-            onLogoutSuccess();
-          }}
-          style={{ marginTop: 20 }}
-        >
-          Logout
-        </Button>
       </Content>
     </Layout>
   );
---
+++ src/OreHistogram.tsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useRef } from "react";
+import { Column } from "@antv/g2plot";
+import dayjs from "dayjs";
+import tailwindColors from "tailwindcss/colors";
+
+const colors = tailwindColors;
+
+export const OresHistogram: React.FC = ({ data }) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const chartRef = useRef<Column>();
+
+  console.log("colors.blue[400]", colors.blue[400]);
+
+  useEffect(() => {
+    if (!containerRef.current) return;
+
+    const timeBuckets = new Map<string, number>();
+
+    data?.forEach(({ timestamp }) => {
+      const day = dayjs.unix(timestamp).format("YYYY-MM-DD");
+      timeBuckets.set(day, (timeBuckets.get(day) || 0) + 1);
+    });
+
+    const histogramData = Array.from(timeBuckets.entries())
+      .map(([time, ores]) => ({
+        time,
+        ores,
+      }))
+      .sort((a, b) => (a.time > b.time ? 1 : -1));
+
+    const histogram = new Column(containerRef.current, {
+      data: histogramData,
+      xField: "time",
+      yField: "ores",
+      xAxis: {
+        title: { text: "Date" },
+        label: {
+          rotate: 45,
+        },
+      },
+      yAxis: {
+        title: { text: "Number of entries" },
+      },
+      tooltip: {
+        fields: ["time", "count"],
+      },
+      //TODO: add this to cause error
+      //   color: `${colors.blue[400]}`,
+    });
+
+    histogram.render();
+    chartRef.current = histogram;
+
+    return () => {
+      chartRef.current?.destroy();
+    };
+  }, [data]);
+
+  return <div ref={containerRef} style={{ width: "100%", height: 400 }} />;
+};
