251092318c72cbb8240d7bd5c394443f5e3495a1
Margo Roi <marharyta.obraztsova@gmail.com>
2025-04-03 00:31:11 +0300

    test: logout flow

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -17,6 +17,28 @@ test.describe("Login flow", () => {
     // Wait for navigation and check for dashboard
     await expect(page.locator("h2")).toHaveText(/dashboard/i);
   });
+
+  test("should logout on click", async ({ page }) => {
+    // // Go to login page
+    await page.goto("http://localhost:5173/");
+    await expect(page.locator("h2")).toHaveText(/login/i);
+    await expect(page.locator("input[id='user_id']")).toBeDefined();
+
+    // Fill in credentials
+    await page.fill('input[id="user_id"]', "alice");
+    await page.fill('input[id="password"]', "1234");
+
+    // Submit the form
+    await page.click('button[type="submit"]');
+
+    // Wait for navigation and check for dashboard
+    await expect(page.locator("h2")).toHaveText(/dashboard/i);
+
+    // Submit the form
+    await page.click('button[type="button"]');
+
+    await expect(page.locator("h2")).toHaveText(/login/i);
+  });
 });
 
 // TODO: create tests to user fetching and data fetching
