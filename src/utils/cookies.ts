cf449418d4a2f31a47d2febf2d8cbbe6579d52e0
Margo Roi <marharyta.obraztsova@gmail.com>
2025-04-17 10:01:49 +0300

    feat: strict coookie policy

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -1,15 +1,14 @@
-export const setCookie = (name: string, value: string, days = 1) => {
+export function setCookie(name: string, value: string, days: number) {
   const expires = new Date(Date.now() + days * 864e5).toUTCString();
   document.cookie = `${name}=${encodeURIComponent(
     value
-  )}; expires=${expires}; path=/`;
-};
-
+  )}; Expires=${expires}; Path=/; Secure; SameSite=Strict`;
+}
 export const getCookie = (name: string): string | null => {
   const match = document.cookie.match("(^|;)\\s*" + name + "\\s*=\\s*([^;]+)");
   return match ? decodeURIComponent(match[2]) : null;
 };
 
-export const deleteCookie = (name: string) => {
-  document.cookie = `${name}=; Max-Age=0; path=/`;
-};
+export function deleteCookie(name: string) {
+  document.cookie = `${name}=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Secure; SameSite=Strict`;
+}
